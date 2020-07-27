33f3a84dce432dbf17ea47e800ecd464f08f599c
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-07-27 11:26:09 -0400

    Init

---
+++ src/main/java/xyz/thebeaver/academy/tbpetclinic/TbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package xyz.thebeaver.academy.tbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/thebeaver/academy/tbpetclinic/TbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.thebeaver.academy.tbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
